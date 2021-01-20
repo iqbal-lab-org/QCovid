9e10e56419e993e4a7d989644f9ecafc3898ad7b
Jeff Knaggs <jknaggs@ebi.ac.uk>
2021-01-20 10:53:55 +0000

    lint + refactor bin_amplicons

--- bin_amplicons.py
+++ bin_amplicons.py
@@ -1,49 +1,63 @@
+"""bin reads by amplicon coordinates
+"""
+
 import sys
 import pysam
 
-amplicons = {}
-amplicon_bed = sys.argv[1]
-reference = "MN908947.3"
-
-amplicon_set = amplicon_bed.split('.bed')[0].split('/')[-1]
-
-for line in open(amplicon_bed):
-    if line[0] == '#':
-        continue
-    fname, start, end = line.strip().split()
-    amplicons[fname] = [int(start), int(end)]
-
-#print(amplicons)
-bam = pysam.AlignmentFile(sys.argv[2], 'rb')
-basename = sys.argv[2].split('.')[0]
-amplicounts = {}
-
-out_name = f"{basename}.{amplicon_set}.bam"
-out_amps = f"{basename}.{amplicon_set}.tsv"
-
-filtered = pysam.AlignmentFile(out_name, 'wb', template=bam)
-total = 0
-for amplicon in amplicons:
-    start, end = amplicons[amplicon]
-
-    # ensure coords are sorted
-    rc = 0
-    for read in bam.fetch(reference, start, end):
-        if not read.reference_start or not read.reference_end:
+def load_amplicons(amplicon_bed):
+    amplicons = {}
+    for line in open(amplicon_bed):
+        if line[0] == '#':
             continue
-        if read.reference_start > (start - 5) and read.reference_end < (end + 5):
-            rc += 1 
-            filtered.write(read)
-            total += 1
-            #if amplicon == 'SARS-CoV-2_36_pool2':
-            #    print(read.reference_start, read.reference_end, read.query_alignment_start, read.query_length, read.query_alignment_sequence)
-    amplicounts[amplicon] = rc
+        fname, start, end = line.strip().split()
+        amplicons[fname] = [int(start), int(end)]
+    return amplicons
 
-filtered.close()
-bam.close()
+def bin_amplicons(reference, amplicons, bam, filtered=None):
+    histogram = {}
+    for amplicon in amplicons:
+        start, end = amplicons[amplicon]
 
-amps_fd = open(out_amps, 'w')
-print('\t'.join([basename, str(total), amplicon_bed, reference]))
-for a in amplicounts:
-    print('\t'.join([a, str(amplicounts[a])]), file=amps_fd)
-amps_fd.close()
+        # ensure coords are sorted
+        rc = 0
+        for read in bam.fetch(reference, start, end):
+            if not read.reference_start or not read.reference_end:
+                continue
+            if read.reference_start > (start - 5) and read.reference_end < (end + 5):
+                rc += 1
+                if filtered:
+                    filtered.write(read)
+                total += 1
+        histogram[amplicon] = rc
+    return histogram
+
+def write_counts(amps_fd, histogram):
+    for a in histogram:
+        print('\t'.join([a, str(histogram[a])]), file=amps_fd)
+    amps_fd.close()
+
+def main():
+    amplicon_bed = sys.argv[1]
+    reference = "MN908947.3"
+
+    amplicon_set = amplicon_bed.split('.bed')[0].split('/')[-1]
+
+    basename = sys.argv[2].split('.')[0]
+
+    out_name = f"{basename}.{amplicon_set}.bam"
+    out_amps = f"{basename}.{amplicon_set}.tsv"
+
+    bam = pysam.AlignmentFile(sys.argv[2], 'rb')
+    filtered = pysam.AlignmentFile(out_name, 'wb', template=bam)
+    amplicons = load_amplicons(amplicon_bed)
+    histogram = bin_amplicons(reference, amplicons, bam, filtered=filtered)
+    total = sum(histogram.values)
+
+    amps_fd = open(out_amps, 'w')
+    summary = '\t'.join([basename, str(total), amplicon_bed, reference])
+    write_counts(amps_fd, histogram)
+    amps_fd.close()
+    print(summary)
+
+if __name__ == "__main__":
+    main()
