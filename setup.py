67d5538a10dd1eab3c9f13b4d88f44e84a2cc469
jeff-k <jeff_k@fastmail.com>
2021-11-12 16:20:23 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
         name='qcovid',
         version='0.1.1',
         description='amplicon based QC data extraction for sars-cov-2',
-        scripts=['qcovid/bin_amplicons.py', 'qcovid/self_qc.py'],
+        scripts=['qcovid/bin_amplicons.py', 'qcovid/self_qc.py', 'qcovid/detect_primers.py'],
 )
